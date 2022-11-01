887624c13d3f1777637b54b04b50f900c7bb9417
leonardo oliveira <leonardo_olivesilva@icloud.com>
2022-10-31 20:28:44 -0700

    Completed

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+        
+
+            string dogName = "Feijao";
+            int age = 2;
+            char firstLetterToy = 'B';
+            bool lovesRunning = true;
+            double fluffyToys = 30;
+            decimal weight = 59.5m;
+            
+            Console.WriteLine($"Hello, my name is {dogName} and I love to play! I am {age} years old and {weight} pounds." +
+                $"It is {lovesRunning} that I love ruuning and that I have {fluffyToys} toys. My favorite thing to chew on starts with the letter {firstLetterToy}.");
+            Console.ReadLine();
         }
     }
 }
